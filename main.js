84fce43114b9fdbb9d624540220024da9b25b5b4
GitHubPlayer0 <65072506+GitHubPlayerZero@users.noreply.github.com>
2024-10-09 17:15:39 +0800

    'Initial'

---
+++ main.js
@@ -0,0 +1,2 @@
+import './assets/scss/all.scss';
+import 'bootstrap';	// 匯入 BS JS
---
+++ vite.config.js
@@ -0,0 +1,55 @@
+import { defineConfig } from 'vite';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { glob } from 'glob';
+
+import liveReload from 'vite-plugin-live-reload';
+
+function moveOutputPlugin() {
+	return {
+		name: 'move-output',
+		enforce: 'post',
+		apply: 'build',
+		async generateBundle(options, bundle) {
+			for (const fileName in bundle) {
+				if (fileName.startsWith('pages/')) {
+					const newFileName = fileName.slice('pages/'.length);
+					bundle[fileName].fileName = newFileName;
+				}
+			}
+		},
+	};
+}
+
+export default defineConfig({
+	// base 的寫法：
+	// base: '/Repository 的名稱/'
+	base: '/sample-vite-layout-bs5-202410/',
+	plugins: [
+		liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
+		ViteEjsPlugin(),
+		moveOutputPlugin(),
+	],
+	//在開發過程中啟用 sourcemap
+	css: {
+		devSourcemap: true,
+	},
+	server: {
+		// 啟動 server 時預設開啟的頁面
+		open: 'pages/index.html',
+	},
+	build: {
+		rollupOptions: {
+			input: Object.fromEntries(
+				glob
+					.sync('pages/**/*.html')
+					.map((file) => [
+						path.relative('pages', file.slice(0, file.length - path.extname(file).length)),
+						fileURLToPath(new URL(file, import.meta.url)),
+					])
+			),
+		},
+		outDir: 'dist',
+	},
+});
